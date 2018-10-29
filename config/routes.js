322760f8f7a338ca8dafa9ab83831489b2a7cac7
JosueDanielBust <josuedanielbustamante@gmail.com>
2018-10-29 18:35:58 -0500

    Adding the base of legal ppages

--- config/routes.js
+++ config/routes.js
@@ -24,10 +24,21 @@ module.exports.routes = {
   *                                                                          *
   ***************************************************************************/
 
+  // General pages
   '/': { view: 'pages/home', locals: { layout: '/layouts/home' } },
+  'get /staff': { view: 'pages/staff', locals: { layout: '/layouts/page', title: 'Staff' } },
+
+  // Shop
   'get /shop': { view: 'pages/shop/main', locals: { layout: '/layouts/page', title: 'Shop' } },
+
+  // Reports
   'get /report': { view: 'pages/report/main', locals: { layout: '/layouts/page', title: 'Report' } },
-  'get /report/thanks': { view: 'pages/report/thanks', locals: { layout: '/layouts/page', title: 'Thank you for your report' } }
+  'get /report/thanks': { view: 'pages/report/thanks', locals: { layout: '/layouts/page', title: 'Thank you for your report' } },
+
+  // Legals
+  'get /privacy-policy': { view: 'pages/privacy-policy', locals: { layout: '/layouts/page', title: 'Privacy Policy' } },
+  'get /terms-of-service': { view: 'pages/terms-of-service', locals: { layout: '/layouts/page', title: 'Terms of Service' } },
+  'get /gdpr': { view: 'pages/gdpr', locals: { layout: '/layouts/page', title: 'GDPR' } },
 
   /***************************************************************************
   *                                                                          *
